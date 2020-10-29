b749a41cbd0116d3aadf4d98293d67bbe046e8c3
Pedro Miguel <pedro_mrmr@hotmail.com>
2020-10-29 01:04:13 +0100

    Added new shortcuts

--- Programs/dwm-6.2/config.h
+++ Programs/dwm-6.2/config.h
@@ -60,16 +60,20 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_magenta, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]   = { "st",           NULL};
+static const char *firefox[]   = {"firefox",       NULL};
+static const char *flameshot[] = {"flameshot","gui",NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,			XK_w,	   spawn,	   {.v = firefox } }, //Shortcut to launch firefox ALT-W
+	{ MODKEY,			XK_Print,  spawn,	   {.v = flameshot } },//Shortcut to print screen ALt+PRNT
+	{ MODKEY,			XK_b,	   togglebar,	   {0} },
+	{ MODKEY,			XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,			XK_k,	   focusstack,     {.i = -1 } },			
+	{ MODKEY,			XK_i,	   incnmaster,	   {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
