656d8d715cfd090c02aa9a9cc40caa05d192a58d
Pedro Miguel <pedro_mrmr@hotmail.com>
2020-11-04 20:33:49 +0100

    Added more items to the statusbar

--- Programs/dwmblock/blocks.h
+++ Programs/dwmblock/blocks.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,		0},
+	/*Icon*/	/*Command*/						/*Update Interval*/   /*Update Signal*/
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",		1,		0},
 
-	{"", "/home/high/Scripts/time.sh",					5,		0},
+	{"", "/home/high/Scripts/time.sh",						5,		0},
 
 	{"","/home/high/Scripts/battery.sh",						30,		0},
+	{"","/home/high/Scripts/disk_usage.sh", 20, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
